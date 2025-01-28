c87079193634377185641bf8ca8aa423ed5a0f01
IH-TUSHAR <mohammodtushar.cb@gmail.com>
2025-01-28 15:50:35 +0600

    github_data_Added

--- index.js
+++ index.js
@@ -3,7 +3,48 @@ require("dotenv").config();
 
 const app = express();
 
-const port = 4000;
+const port = 3000;
+
+const Github_Data = {
+  login: "IH-Tushar",
+  id: 89769117,
+  node_id: "MDQ6VXNlcjg5NzY5MTE3",
+  avatar_url: "https://avatars.githubusercontent.com/u/89769117?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/IH-Tushar",
+  html_url: "https://github.com/IH-Tushar",
+  followers_url: "https://api.github.com/users/IH-Tushar/followers",
+  following_url:
+    "https://api.github.com/users/IH-Tushar/following{/other_user}",
+  gists_url: "https://api.github.com/users/IH-Tushar/gists{/gist_id}",
+  starred_url: "https://api.github.com/users/IH-Tushar/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/IH-Tushar/subscriptions",
+  organizations_url: "https://api.github.com/users/IH-Tushar/orgs",
+  repos_url: "https://api.github.com/users/IH-Tushar/repos",
+  events_url: "https://api.github.com/users/IH-Tushar/events{/privacy}",
+  received_events_url: "https://api.github.com/users/IH-Tushar/received_events",
+  type: "User",
+  user_view_type: "public",
+  site_admin: false,
+  name: "IH-TUSHAR",
+  company: null,
+  blog: "",
+  location: null,
+  email: null,
+  hireable: null,
+  bio: null,
+  twitter_username: null,
+  public_repos: 31,
+  public_gists: 0,
+  followers: 3,
+  following: 7,
+  created_at: "2021-08-30T07:06:03Z",
+  updated_at: "2025-01-10T16:52:53Z",
+};
+
+app.get("/github", (req, res) => {
+  res.json(Github_Data);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -20,6 +61,6 @@ app.get("/google", (req, res) => {
 //   res.redirect("https://www.youtube.com/");
 // });
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
